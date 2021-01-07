b8c39799d79fc61f25343e3355744b25d94a0917
Anand Choudhary <raschoudhary879@gmail.com>
2021-01-07 19:43:48 +0530

    first commit

---
+++ p1.js
@@ -0,0 +1,52 @@
+
+//navigation menu
+
+function openmenu(){
+  var x=document.getElementById("navigation_menu");
+  if(x.style.display==="none")
+  {
+    x.style.display="block";
+    document.getElementById("ham").style.display="none";
+    document.getElementById("close_ham").style.display="block";
+  }
+  else
+  {
+    x.style.display="none";
+    document.getElementById("ham").style.display="block";
+    document.getElementById("close_ham").style.display="none";
+  }
+}
+
+//logo_menu info move
+
+var slideinfo_count = 0;
+showinfo();
+
+async function showinfo() {
+  var i;
+  var slides = document.getElementsByClassName("info123")
+  for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+  }
+  slideinfo_count++;
+  if (slideinfo_count > slides.length) {slideinfo_count = 1}
+  slides[slideinfo_count-1].style.display = "block";
+  setTimeout(showinfo, 2000); // Change image every 2 seconds
+}
+
+//moving_project move
+
+var slideIndex = 0;
+showslide();
+
+async function showslide() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides")
+  for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "flex";
+  setTimeout(showslide, 3000); // Change image every 5 seconds
+}
